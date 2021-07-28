e9a2ad8be2837ecb08e7f65f5b4d5d0c10536b4b
andres sanchez <andres.camilo980810@gmail.com>
2021-07-27 19:30:36 -0500

    alertas y ventanas de ingreso

---
+++ practica/js/alertas_y_ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+// ventanas
+
+alert("this is a window");
+
+//confirm("¿estas seguro que deceas continuar?");
+
+var resultado = confirm("¿estas seguro que deceas continuar?");
+
+document.write(resultado);
+
+var edad= prompt("¿Que edad tienes?", 18);
